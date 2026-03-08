3a1bb916830e827980b19f4498144359133b12f9
Mouhcine <mouhcine@gmail.com>
2026-03-08 02:22:33 +0100

    premier api python

---
+++ shiftheroes_request.py
@@ -0,0 +1,7 @@
+import requests
+
+headers = {
+    'Authorization': 'Bearer 6aacff70da40d3b90dc44b9790be2188\n\n',
+}
+
+response = requests.post('https://shiftheroes.fr/api/v1/plannings/wf7nw/shifts/rYFk3gE/reservations', headers=headers)
